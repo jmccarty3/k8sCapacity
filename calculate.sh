c6ef68b82bca179ead90af3641609c28c26a1ed5
Justin McCarty <jmccarty@ip-192-168-1-5.ec2.internal>
2016-05-10 08:28:31 -0400

    Adding 30 second max time to all curl calls

--- calculate.sh
+++ calculate.sh
@@ -2,7 +2,7 @@
 set -e
 
 function write_data {
-  curl -s -i -XPOST $INFLUXDB_ADDRESS/write?db=$db --data-binary "$BATCH_DATA"
+  curl -m 30 -s -i -XPOST $INFLUXDB_ADDRESS/write?db=$db --data-binary "$BATCH_DATA"
 }
 
 function fetch_node_capacity {
@@ -31,9 +31,9 @@ export BEARER_TOKEN=/var/run/secrets/kubernetes.io/serviceaccount/token
 
 if [ -f $BEARER_TOKEN ]; then
   TOKEN=$(cat $BEARER_TOKEN)
-  CURL_CMD="curl -s --header \"Authorization: Bearer $BEARER_TOKEN\" --insecure https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT"
+  CURL_CMD="curl -m 30 -s --header \"Authorization: Bearer $BEARER_TOKEN\" --insecure https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT"
 elif [ -n "$KUBERNETES_MASTER" ]; then
-  CURL_CMD="curl -s --insecure http://$KUBERNETES_MASTER"
+  CURL_CMD="curl -m 30 -s --insecure http://$KUBERNETES_MASTER"
 else
   echo "Unable to find a service token or KUBERNETES_MASTER variable"
   exit -1
